1ab2423b483f555bb558a149e1c845bc1f01884f
Christian Persch <chpe@src.gnome.org>
2003-08-03 23:20:35 +0000

    Adding ephy-file-helpers.h

---
+++ include/ephy-file-helpers.h
@@ -0,0 +1,55 @@
+/*
+ *  Copyright (C) 2002 Jorn Baayen
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#ifndef EPHY_FILE_HELPERS_H
+#define EPHY_FILE_HELPERS_H
+
+#include <glib.h>
+#include <libxml/tree.h>
+
+G_BEGIN_DECLS
+
+const char *ephy_file                    (const char *filename);
+
+const char *ephy_dot_dir                 (void);
+
+void        ephy_file_helpers_init       (void);
+
+void        ephy_file_helpers_shutdown   (void);
+
+void	    ephy_file_launch_application (const char *command_string,
+					  const char *parameter,
+					  gboolean use_terminal);
+
+char       *ephy_file_tmp_filename	 (const char *base,
+					  const char *extension);
+
+void	    ephy_ensure_dir_exists       (const char *dir);
+
+GSList     *ephy_file_find               (const char *path,
+	                                  const char *fname,
+	                                  gint maxdepth);
+
+gboolean    ephy_file_save_xml           (const char *xml_file,
+					  xmlDocPtr doc);
+
+G_END_DECLS
+
+#endif /* EPHY_FILE_HELPERS_H */
