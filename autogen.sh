2a7b58cfeb099b45a355aca052acc7c471a5e1c8
Christian Persch <chpe@src.gnome.org>
2003-11-12 20:17:30 +0000

    Updating

--- autogen.sh
+++ autogen.sh
@@ -4,9 +4,9 @@
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PKG_NAME="ephy-plugins"
+PKG_NAME="epiphany-extensions"
 
-(test -f $srcdir/configure.in) || {
+(test -f $srcdir/configure.ac) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
     exit 1
