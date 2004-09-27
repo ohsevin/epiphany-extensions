a9e5052c2d3bf264f8d778f9d33005f5ed9960a3
Christian Persch <chpe@cvs.gnome.org>
2004-09-27 16:58:21 +0000

    A extensions/sidebar/Makefile.am: A
    2004-09-27  Christian Persch  <chpe@cvs.gnome.org>
    
    	* AUTHORS:
    	* configure.ac:
    	* extensions/Makefile.am:
    	A extensions/sidebar/Makefile.am:
    	A extensions/sidebar/ephy-sidebar-embed.c:
    	A extensions/sidebar/ephy-sidebar-embed.h:
    	A extensions/sidebar/ephy-sidebar-extension.c:
    	A extensions/sidebar/ephy-sidebar-extension.h:
    	A extensions/sidebar/ephy-sidebar-marshal.list:
    	A extensions/sidebar/ephy-sidebar.c:
    	A extensions/sidebar/ephy-sidebar.h:
    	A extensions/sidebar/epiphany-sidebar-ui.xml:
    	A extensions/sidebar/extension.c: (register_module):
    	A extensions/sidebar/mozilla/Makefile.am:
    	A extensions/sidebar/mozilla/Sidebar.cpp:
    	A extensions/sidebar/mozilla/Sidebar.h:
    	A extensions/sidebar/mozilla/register-component.cpp:
    	A extensions/sidebar/mozilla/register-component.h:
    	A extensions/sidebar/sidebar-commands.c: (get_event_info),
    	A extensions/sidebar/sidebar-commands.h:
    	* include/Makefile.am:
    	A include/ephy-prefs.h:
    	* po/POTFILES.in:
    	* po/POTFILES.skip:
    
    	Added "Sidebar" extension, written by Crispin Flowerday.

---
+++ extensions/sidebar/extension.c
@@ -0,0 +1,52 @@
+/*
+ *  Copyright (C) 2003 Marco Pesenti Gritti
+ *  Copyright (C) 2003 Christian Persch
+ *  Copyright (C) 2004 Crispin Flowerday
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "ephy-sidebar-extension.h"
+#include "ephy-sidebar-embed.h"
+#include "ephy-sidebar.h"
+#include "ephy-debug.h"
+
+#include <gmodule.h>
+#include <glib/gi18n-lib.h>
+
+G_MODULE_EXPORT GType register_module (GTypeModule *module);
+
+G_MODULE_EXPORT GType
+register_module (GTypeModule *module)
+{
+	LOG ("Registering EphySidebarExtension")
+
+#ifdef ENABLE_NLS
+       /* Initialise the i18n stuff */
+	bindtextdomain (GETTEXT_PACKAGE, EPHY_EXTENSIONS_LOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");	
+#endif /* ENABLE_NLS */
+
+	ephy_sidebar_register_type (module);
+	ephy_sidebar_embed_register_type (module);
+
+	return ephy_sidebar_extension_register_type (module);
+}
---
+++ extensions/sidebar/sidebar-commands.h
@@ -0,0 +1,45 @@
+/*
+ *  Copyright (C) 2004 Crispin Flowerday
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#ifndef SIDEBAR_COMMANDS_H
+#define SIDEBAR_COMMANDS_H
+
+#include <gtk/gtkaction.h>
+
+#include "ephy-sidebar-embed.h"
+
+G_BEGIN_DECLS
+
+void sidebar_cmd_file_save_as		(GtkAction *action,
+					 EphySidebarEmbed *sidebar);
+void sidebar_cmd_save_background_as	(GtkAction *action,
+					 EphySidebarEmbed *sidebar);
+void sidebar_cmd_open_frame		(GtkAction *action,
+					 EphySidebarEmbed *sidebar);
+void sidebar_cmd_download_link		(GtkAction *action,
+					 EphySidebarEmbed *window);
+void sidebar_cmd_download_link_as	(GtkAction *action,
+					 EphySidebarEmbed *window);
+void sidebar_cmd_save_image_as		(GtkAction *action,
+					 EphySidebarEmbed *window);
+
+G_END_DECLS
+
+#endif
