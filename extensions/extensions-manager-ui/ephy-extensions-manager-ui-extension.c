a63d7d451ffb0f786b62387731bdb756e0694e75
Christian Persch <chpe@cvs.gnome.org>
2004-10-13 22:17:09 +0000

    Make dialogue transient to the opening window.
    2004-10-14  Christian Persch  <chpe@cvs.gnome.org>
    
    	* extensions/extensions-manager-ui/ephy-extensions-manager-ui-exten
    	sion.c: (ephy_extensions_manager_ui_extension_get_type),
    	(ephy_extensions_manager_ui_extension_register_type),
    	(ephy_extensions_manager_ui_extension_finalize), (display_cb),
    	(impl_attach_window),
    	(ephy_extensions_manager_ui_extension_class_init):
    
    	Make dialogue transient to the opening window.

--- extensions/extensions-manager-ui/ephy-extensions-manager-ui-extension.c
+++ extensions/extensions-manager-ui/ephy-extensions-manager-ui-extension.c
@@ -37,7 +37,7 @@
 
 #include <glib/gi18n-lib.h>
 
-#define WINDOW_DATA_KEY "EphyExtensionsManagerUIExtensionWindowData"
+#define WINDOW_DATA_KEY "EEMUIEWindowData"
 
 #define EPHY_EXTENSIONS_MANAGER_UI_EXTENSION_GET_PRIVATE(object)(G_TYPE_INSTANCE_GET_PRIVATE ((object), EPHY_TYPE_EXTENSIONS_MANAGER_UI_EXTENSION, EphyExtensionsManagerUIExtensionPrivate))
 
@@ -52,12 +52,13 @@ static void ephy_extensions_manager_ui_extension_init		(EphyExtensionsManagerUIE
 
 typedef struct
 {
+	EphyExtensionsManagerUIExtension *extension;
 	GtkActionGroup *action_group;
 	guint ui_id;
 } WindowData;
 
 static void display_cb (GtkAction *action,
-			EphyExtensionsManagerUIExtension *extension);
+			EphyWindow *window);
 
 static GtkActionEntry action_entries [] =
 {
@@ -68,16 +69,15 @@ static GtkActionEntry action_entries [] =
 	  N_("Load and unload extensions"),
 	  G_CALLBACK (display_cb) },
 };
-static const guint n_action_entries = G_N_ELEMENTS (action_entries);
 
-static GObjectClass *extensions_manager_ui_extension_parent_class = NULL;
+static GObjectClass *parent_class = NULL;
 
-static GType extensions_manager_ui_extension_type = 0;
+static GType type = 0;
 
 GType
 ephy_extensions_manager_ui_extension_get_type (void)
 {
-	return extensions_manager_ui_extension_type;
+	return type;
 }
 
 GType
@@ -103,17 +103,16 @@ ephy_extensions_manager_ui_extension_register_type (GTypeModule *module)
 		NULL
 	};
 
-	extensions_manager_ui_extension_type =
-		g_type_module_register_type (module,
-					     G_TYPE_OBJECT,
-					     "EphyExtensionsManagerUIExtension",
-					     &our_info, 0);
+	type = g_type_module_register_type (module,
+					    G_TYPE_OBJECT,
+					    "EphyExtensionsManagerUIExtension",
+					    &our_info, 0);
 
 	g_type_module_add_interface (module,
-				     extensions_manager_ui_extension_type,
+				     type,
 				     EPHY_TYPE_EXTENSION,
 				     &extension_info);
-	return extensions_manager_ui_extension_type;
+	return type;
 }
 
 static void
@@ -138,56 +137,61 @@ ephy_extensions_manager_ui_extension_finalize (GObject *object)
 		g_object_unref (extension->priv->dialog);
 	}
 
-	G_OBJECT_CLASS (extensions_manager_ui_extension_parent_class)->finalize (object);
-}
-
-static void
-free_window_data (WindowData *data)
-{
-	g_object_unref (data->action_group);
-	g_free (data);
+	parent_class->finalize (object);
 }
 
 static void
 display_cb (GtkAction *action,
-	    EphyExtensionsManagerUIExtension *extension)
+	    EphyWindow *window)
 {
-	if (extension->priv->dialog == NULL)
+	EphyExtensionsManagerUIExtensionPrivate *priv;
+	WindowData *data;
+
+	data = g_object_get_data (G_OBJECT (window), WINDOW_DATA_KEY);
+	g_return_if_fail (data != NULL);
+
+	priv = data->extension->priv;
+
+	if (priv->dialog == NULL)
 	{
-		extension->priv->dialog = extensions_manager_ui_new ();
-		g_object_add_weak_pointer (G_OBJECT (extension->priv->dialog),
-					   (gpointer *) &extension->priv->dialog);
+		priv->dialog = extensions_manager_ui_new ();
+		g_object_add_weak_pointer (G_OBJECT (priv->dialog),
+					   (gpointer *) &priv->dialog);
 	}
 
-	ephy_dialog_show (EPHY_DIALOG (extension->priv->dialog));
+	ephy_dialog_set_parent (EPHY_DIALOG (priv->dialog),
+				GTK_WIDGET (window));
+	ephy_dialog_show (EPHY_DIALOG (priv->dialog));
 }
 
 static void
 impl_attach_window (EphyExtension *ext,
 		    EphyWindow *window)
 {
+	EphyExtensionsManagerUIExtension *extension = EPHY_EXTENSIONS_MANAGER_UI_EXTENSION (ext);
 	GtkActionGroup *action_group;
 	GtkUIManager *manager;
 	guint ui_id;
 	WindowData *data;
-	EphyExtensionsManagerUIExtension *extension = EPHY_EXTENSIONS_MANAGER_UI_EXTENSION (ext);
 
 	data = g_new (WindowData, 1);
 
 	manager = GTK_UI_MANAGER (ephy_window_get_ui_manager (window));
 
-	data->action_group = action_group =
-		gtk_action_group_new ("EphyExtensionsManagerUIExtensionActions");
+	data->extension = extension;
+
+	data->action_group = action_group = gtk_action_group_new ("EEMUIEActions");
 	gtk_action_group_set_translation_domain (action_group, GETTEXT_PACKAGE);
 	gtk_action_group_add_actions (action_group, action_entries,
-				      n_action_entries, extension);
+				      G_N_ELEMENTS (action_entries), window);
 
 	gtk_ui_manager_insert_action_group (manager, action_group, -1);
+	g_object_unref (action_group);
 
 	data->ui_id = ui_id = gtk_ui_manager_new_merge_id (manager);
 
 	g_object_set_data_full (G_OBJECT (window), WINDOW_DATA_KEY, data,
-				(GDestroyNotify) free_window_data);
+				(GDestroyNotify) g_free);
 
 	gtk_ui_manager_add_ui (manager, ui_id, "/menubar/ToolsMenu",
 			       "ExtensionsManagerUISep", NULL,
@@ -235,7 +239,7 @@ ephy_extensions_manager_ui_extension_class_init (EphyExtensionsManagerUIExtensio
 {
 	GObjectClass *object_class = G_OBJECT_CLASS (klass);
 
-	extensions_manager_ui_extension_parent_class = g_type_class_peek_parent (klass);
+	parent_class = g_type_class_peek_parent (klass);
 
 	object_class->finalize = ephy_extensions_manager_ui_extension_finalize;
 
