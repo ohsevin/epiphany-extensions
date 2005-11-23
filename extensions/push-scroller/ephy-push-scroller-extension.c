5a2d7d0d9fabd60ba75db3aba81d3e09da6ff37b
Christian Persch <chpe@cvs.gnome.org>
2005-11-23 22:20:47 +0000

    A extensions/push-scroller/.cvsignore: A
    2005-11-23  Christian Persch  <chpe@cvs.gnome.org>
    
    	* AUTHORS:
    	* Makefile.am:
    	* configure.ac:
    	A extensions/push-scroller/.cvsignore:
    	A extensions/push-scroller/Makefile.am:
    	A extensions/push-scroller/ephy-push-scroller-extension.c:
    	A extensions/push-scroller/ephy-push-scroller-extension.h:
    	A extensions/push-scroller/ephy-push-scroller.c:
    	A extensions/push-scroller/ephy-push-scroller.h:
    	A extensions/push-scroller/extension.c:
    	A extensions/push-scroller/mozilla/.cvsignore:
    	A extensions/push-scroller/mozilla/Makefile.am:
    	A extensions/push-scroller/mozilla/mozilla-helpers.cpp:
    	A extensions/push-scroller/mozilla/mozilla-helpers.h:
    	A extensions/push-scroller/push-scroller.ephy-extension.in.in:
    
    	Add "Push Scroller" extension. Based on the Auto Scroller extension,
    	modified by Samuel Abels. Bug #322275.

---
+++ extensions/push-scroller/ephy-push-scroller-extension.c
@@ -0,0 +1,174 @@
+/*
+ *  Copyright (C) 2003 Marco Pesenti Gritti
+ *  Copyright (C) 2003, 2005 Christian Persch
+ *  Copyright (C) 2005 Crispin Flowerday
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License scrollerpublished by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#include "config.h"
+
+#include "ephy-push-scroller-extension.h"
+#include "ephy-push-scroller.h"
+#include "mozilla-helpers.h"
+#include "ephy-debug.h"
+
+#include <epiphany/ephy-extension.h>
+
+#include <gmodule.h>
+
+#define WINDOW_DATA_KEY	"EphyPushScrollerExtension::WindowData"
+
+static GType type = 0;
+
+static void
+ephy_push_scroller_extension_init (EphyPushScrollerExtension *extension)
+{
+}
+
+static EphyPushScroller *
+ensure_push_scroller (EphyWindow *window)
+{
+	EphyPushScroller *scroller;
+
+	scroller = g_object_get_data (G_OBJECT (window), WINDOW_DATA_KEY);
+	if (scroller == NULL)
+	{
+		scroller = ephy_push_scroller_new (window);
+		g_object_set_data_full (G_OBJECT (window), WINDOW_DATA_KEY,
+					scroller,
+					(GDestroyNotify) g_object_unref);
+	}
+
+	return scroller;
+}
+
+static gboolean
+dom_mouse_down_cb (EphyEmbed *embed,
+		   EphyEmbedEvent *event,
+		   EphyWindow *window)
+{
+	EphyPushScroller *scroller;
+	EphyEmbedEventContext context;
+	guint button, x, y;
+
+	button = ephy_embed_event_get_button (event);
+	context = ephy_embed_event_get_context (event);
+	
+	if (button != 2 || (context & EPHY_EMBED_CONTEXT_INPUT) ||
+            (context & EPHY_EMBED_CONTEXT_LINK))
+	{
+		return FALSE;
+	}
+
+	scroller = ensure_push_scroller (window);
+	g_return_val_if_fail (scroller != NULL, FALSE);
+
+	ephy_embed_event_get_coords (event, &x, &y);
+	ephy_push_scroller_start (scroller, embed, x, y);
+
+	return TRUE;
+}
+
+static void
+impl_detach_window (EphyExtension *ext,
+                    EphyWindow *window)
+{
+        g_object_set_data (G_OBJECT (window), WINDOW_DATA_KEY, NULL);
+}
+
+static void
+impl_attach_tab (EphyExtension *ext,
+		 EphyWindow *window,
+		 EphyTab *tab)
+{
+	EphyEmbed *embed;
+
+	LOG ("impl_attach_tab");
+
+	embed = ephy_tab_get_embed (tab);
+	g_return_if_fail (embed != NULL);
+
+	g_signal_connect_object (embed, "ge-dom-mouse-down",
+                                 G_CALLBACK (dom_mouse_down_cb), window, 0);
+}
+
+static void
+impl_detach_tab (EphyExtension *ext,
+		 EphyWindow *window,
+		 EphyTab *tab)
+{
+	EphyEmbed *embed;
+
+	LOG ("impl_detach_tab");
+
+	embed = ephy_tab_get_embed (tab);
+	g_return_if_fail (embed != NULL);
+
+	g_signal_handlers_disconnect_by_func
+		(embed, G_CALLBACK (dom_mouse_down_cb), window);
+}
+
+static void
+ephy_push_scroller_extension_iface_init (EphyExtensionIface *iface)
+{
+	iface->detach_window = impl_detach_window;
+	iface->attach_tab = impl_attach_tab;
+	iface->detach_tab = impl_detach_tab;
+}
+
+GType
+ephy_push_scroller_extension_get_type (void)
+{
+	return type;
+}
+
+GType
+ephy_push_scroller_extension_register_type (GTypeModule *module)
+{
+	static const GTypeInfo our_info =
+	{
+		sizeof (EphyPushScrollerExtensionClass),
+		NULL, /* base_init */
+		NULL, /* base_finalize */
+		NULL, /* class_init */
+		NULL, /* class_finalize */
+		NULL, /* class_data */
+		sizeof (EphyPushScrollerExtension),
+		0, /* n_preallocs */
+		(GInstanceInitFunc) ephy_push_scroller_extension_init
+	};
+
+	static const GInterfaceInfo extension_info =
+	{
+		(GInterfaceInitFunc) ephy_push_scroller_extension_iface_init,
+		NULL,
+		NULL
+	};
+
+	type = g_type_module_register_type (module,
+					    G_TYPE_OBJECT,
+					    "EphyPushScrollerExtension",
+					    &our_info, 0);
+
+	g_type_module_add_interface (module,
+				     type,
+				     EPHY_TYPE_EXTENSION,
+				     &extension_info);
+
+	return type;
+}
---
+++ extensions/push-scroller/ephy-push-scroller.c
@@ -0,0 +1,388 @@
+/*
+ *  Copyright (C) 2002 Ricardo Fernádez Pascual
+ *  Copyright (C) 2005 Crispin Flowerday
+ *  Copyright (C) 2005 Christian Persch
+ *  Copyright (C) 2005 Samuel Abels
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+/* this file is based on work of Daniel Erat for galeon 1 */
+
+#include "config.h"
+
+#include "ephy-push-scroller.h"
+#include "mozilla-helpers.h"
+#include "ephy-debug.h"
+
+#include <gtk/gtkimage.h>
+#include <gtk/gtkwindow.h>
+#include <gtk/gtkmain.h>
+
+#include <math.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define EPHY_PUSH_SCROLLER_GET_PRIVATE(object) (G_TYPE_INSTANCE_GET_PRIVATE ((object), EPHY_TYPE_PUSH_SCROLLER, EphyPushScrollerPrivate))
+
+struct _EphyPushScrollerPrivate
+{
+	EphyWindow *window;
+	EphyEmbed *embed;
+	GdkCursor *cursor;
+	guint start_x;
+	guint start_y;
+	guint active : 1;
+};
+
+enum
+{
+	PROP_0,
+	PROP_WINDOW
+};
+
+static GObjectClass *parent_class = NULL;
+
+static GType type = 0;
+
+/* private functions */
+
+static void
+ephy_push_scroller_set_window (EphyPushScroller *scroller,
+			       EphyWindow *window)
+{
+	EphyPushScrollerPrivate *priv = scroller->priv;
+
+	priv->window = window;
+}
+
+static gboolean
+ephy_push_scroller_motion_cb (GtkWidget *widget,
+			      GdkEventMotion *event,
+			      EphyPushScroller *scroller)
+{
+	EphyPushScrollerPrivate *priv = scroller->priv;
+	int x_dist, x_dist_abs, y_dist, y_dist_abs;
+
+	if (!priv->active)
+	{
+		return FALSE;
+	}
+
+	/* get distance between last known cursor position and cursor */
+	x_dist = priv->start_x - event->x_root;
+	x_dist_abs = abs (x_dist);
+	y_dist = priv->start_y - event->y_root;
+	y_dist_abs = abs (y_dist);
+
+	/* scroll */
+	mozilla_helper_fine_scroll (priv->embed, x_dist, y_dist);
+
+	priv->start_x = event->x_root;
+	priv->start_y = event->y_root;
+
+	return TRUE;
+}
+
+static gboolean
+ephy_push_scroller_mouse_press_cb (GtkWidget *widget,
+				   GdkEventButton *event,
+				   EphyPushScroller *scroller)
+{
+	ephy_push_scroller_stop (scroller, event->time);
+
+	return TRUE;
+}
+
+static gboolean
+ephy_push_scroller_mouse_release_cb (GtkWidget *widget,
+				   GdkEventButton *event,
+				   EphyPushScroller *scroller)
+{
+	ephy_push_scroller_stop (scroller, event->time);
+
+	return TRUE;
+}
+
+static gboolean
+ephy_push_scroller_key_press_cb (GtkWidget *widget,
+				 GdkEventKey *event,
+				 EphyPushScroller *scroller)
+{
+	ephy_push_scroller_stop (scroller, event->time);
+
+	return TRUE;
+}
+
+/* FIXME: I think this is a subcase of grab-broken now? */
+static gboolean
+ephy_push_scroller_unmap_event_cb (GtkWidget *widget,
+				   GdkEventAny *event,
+				   EphyPushScroller *scroller)
+{
+	ephy_push_scroller_stop (scroller, gtk_get_current_event_time ());
+
+	return FALSE;
+}
+
+static gboolean
+ephy_push_scroller_grab_broken_event_cb (GtkWidget *widget,
+					 GdkEventGrabBroken *event,
+					 EphyPushScroller *scroller)
+{
+	LOG ("Grab Broken [%p, window %p]", scroller, scroller->priv->window);
+
+	ephy_push_scroller_stop (scroller, GDK_CURRENT_TIME /* FIXME? */);
+
+	return FALSE;
+}
+
+static void
+ephy_push_scroller_grab_notify_cb (GtkWidget *widget,
+				   gboolean was_grabbed,
+				   EphyPushScroller *scroller)
+{
+	LOG ("Grab Notify [%p, window %p]", scroller, scroller->priv->window);
+
+	ephy_push_scroller_stop (scroller, GDK_CURRENT_TIME /* FIXME? */);
+}
+
+/* public functions */
+
+void
+ephy_push_scroller_start (EphyPushScroller *scroller,
+			  EphyEmbed *embed,
+			  int x,
+			  int y)
+{
+	EphyPushScrollerPrivate *priv = scroller->priv;
+	GtkWidget *widget, *child;
+	guint32 timestamp;
+
+	g_return_if_fail (embed != NULL);
+
+	LOG ("Start [%p]", scroller);
+
+	if (priv->active) return;
+
+	if (gdk_pointer_is_grabbed ()) return;
+
+	priv->active = TRUE;
+
+	/* FIXME is this good enough? */
+	timestamp = gtk_get_current_event_time ();
+
+	g_object_ref (scroller);
+
+	priv->embed = embed;
+
+	g_object_ref (priv->window);
+
+	/* set positions */
+	priv->start_x = x;
+	priv->start_y = y;
+
+	g_signal_connect (priv->window, "motion-notify-event",
+			    G_CALLBACK (ephy_push_scroller_motion_cb), scroller);
+	g_signal_connect (priv->window, "button-press-event",
+			  G_CALLBACK (ephy_push_scroller_mouse_press_cb), scroller);
+	g_signal_connect (priv->window, "button-release-event",
+			  G_CALLBACK (ephy_push_scroller_mouse_release_cb), scroller);
+	g_signal_connect (priv->window, "key-press-event",
+			  G_CALLBACK (ephy_push_scroller_key_press_cb), scroller);
+	g_signal_connect (priv->window, "unmap-event",
+			  G_CALLBACK (ephy_push_scroller_unmap_event_cb), scroller);
+	g_signal_connect (priv->window, "grab-broken-event",
+			  G_CALLBACK (ephy_push_scroller_grab_broken_event_cb), scroller);
+
+	/* FIXME: this signal only seems to be emitted on the container children of GtkWindow,
+	 * not on GtkWindow itself... is there a better way to get notified of new grabs?
+	 */
+	child = gtk_bin_get_child (GTK_BIN (priv->window));
+	g_signal_connect_object (child, "grab-notify",
+				 G_CALLBACK (ephy_push_scroller_grab_notify_cb), scroller, 0);
+
+	/* grab the pointer */
+	widget = GTK_WIDGET (priv->window);
+	gtk_grab_add (widget);
+	if (gdk_pointer_grab (widget->window, FALSE,
+			      GDK_POINTER_MOTION_MASK |
+			      GDK_BUTTON_PRESS_MASK |
+			      GDK_BUTTON_RELEASE_MASK,
+			      NULL, priv->cursor, timestamp) != GDK_GRAB_SUCCESS)
+	{
+		ephy_push_scroller_stop (scroller, timestamp);
+		return;
+	}
+
+	if (gdk_keyboard_grab (widget->window, FALSE, timestamp) != GDK_GRAB_SUCCESS)
+	{
+		ephy_push_scroller_stop (scroller, timestamp);
+		return;
+	}
+}
+
+void
+ephy_push_scroller_stop (EphyPushScroller *scroller,
+			 guint32 timestamp)
+{
+	EphyPushScrollerPrivate *priv = scroller->priv;
+	GtkWidget *widget;
+
+	LOG ("Stop [%p]", scroller);
+
+	if (priv->active == FALSE) return;
+
+	/* disconnect the signals before ungrabbing! */
+	g_signal_handlers_disconnect_matched (priv->window,
+					      G_SIGNAL_MATCH_DATA, 0, 0, 
+					      NULL, NULL, scroller);
+	g_signal_handlers_disconnect_matched (gtk_bin_get_child (GTK_BIN (priv->window)),
+					      G_SIGNAL_MATCH_DATA, 0, 0, 
+					      NULL, NULL, scroller);
+
+	/* ungrab the pointer if it's grabbed */
+	if (gdk_pointer_is_grabbed ())
+	{
+		gdk_pointer_ungrab (timestamp);
+	}
+
+	gdk_keyboard_ungrab (timestamp);
+
+	widget = GTK_WIDGET (priv->window);
+	gtk_grab_remove (widget);
+
+	g_object_unref (priv->window);
+
+	priv->embed = NULL;
+	priv->active = FALSE;
+
+	g_object_unref (scroller);
+}
+
+EphyPushScroller *
+ephy_push_scroller_new (EphyWindow *window)
+{
+	return EPHY_PUSH_SCROLLER (g_object_new (EPHY_TYPE_PUSH_SCROLLER,
+				    		 "window", window,
+						 NULL));
+}
+
+/* class implementation */
+
+static void 
+ephy_push_scroller_init (EphyPushScroller *scroller)
+{
+	EphyPushScrollerPrivate *priv;
+	priv = scroller->priv = EPHY_PUSH_SCROLLER_GET_PRIVATE (scroller);
+	priv->active = FALSE;
+	priv->cursor = gdk_cursor_new (GDK_FLEUR);
+}
+
+static void
+ephy_push_scroller_finalize (GObject *object)
+{
+	EphyPushScroller *scroller = EPHY_PUSH_SCROLLER (object);
+	EphyPushScrollerPrivate *priv = scroller->priv;
+
+	LOG ("Finalize [%p]", object);
+
+	gdk_cursor_unref (priv->cursor);
+
+	parent_class->finalize (object);
+}
+
+static void
+ephy_push_scroller_set_property (GObject *object,
+				 guint prop_id,
+				 const GValue *value,
+				 GParamSpec *pspec)
+{
+	EphyPushScroller *scroller = EPHY_PUSH_SCROLLER (object);
+
+	switch (prop_id)
+	{
+		case PROP_WINDOW:
+			ephy_push_scroller_set_window (scroller, g_value_get_object (value));
+			break;
+	}
+}
+
+static void
+ephy_push_scroller_get_property (GObject *object,
+				 guint prop_id,
+				 GValue *value,
+				 GParamSpec *pspec)
+{
+	/* no readable properties */
+	g_return_if_reached ();
+}
+
+static void
+ephy_push_scroller_class_init (EphyPushScrollerClass *klass)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+	parent_class = g_type_class_peek_parent (klass);
+
+	object_class->finalize = ephy_push_scroller_finalize;
+	object_class->set_property = ephy_push_scroller_set_property;
+	object_class->get_property = ephy_push_scroller_get_property;
+
+	g_object_class_install_property
+		(object_class,
+		 PROP_WINDOW,
+		 g_param_spec_object ("window",
+				      "window",
+				      "window",
+				      EPHY_TYPE_WINDOW,
+				      G_PARAM_WRITABLE | G_PARAM_CONSTRUCT_ONLY));
+	
+
+	g_type_class_add_private (klass, sizeof (EphyPushScrollerPrivate));
+}
+
+GType
+ephy_push_scroller_get_type (void)
+{
+	return type;
+}
+
+GType
+ephy_push_scroller_register_type (GTypeModule *module)
+{
+	static const GTypeInfo our_info =
+	{
+		sizeof (EphyPushScrollerClass),
+		NULL, /* base_init */
+		NULL, /* base_finalize */
+		(GClassInitFunc) ephy_push_scroller_class_init,
+		NULL,
+		NULL, /* class_data */
+		sizeof (EphyPushScroller),
+		0, /* n_preallocs */
+		(GInstanceInitFunc) ephy_push_scroller_init
+	};
+
+	type = g_type_module_register_type (module,
+					    G_TYPE_OBJECT,
+					    "EphyPushScroller",
+					    &our_info, 0);
+
+	return type;
+}
