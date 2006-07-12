c38f80954add8baaf456cf5a4647d7fcce54ca35
Jean-François Rameau <jframeau@cvs.gnome.org>
2006-07-12 21:16:38 +0000

    New Live HTTP Headers extension.
    2006-07-12  Jean-François Rameau  <jframeau@cvs.gnome.org>
    
    	* README:
    	* configure.ac:
    	* extensions/livehttpheaders/ephy-livehttpheaders-extension.c:
    	* extensions/livehttpheaders/ephy-livehttpheaders-extension.h:
    	* extensions/livehttpheaders/extension.c:
    	* extensions/livehttpheaders/livehttpheaders.ephy-extension.in.in:
    	* extensions/livehttpheaders/livehttpheaders-ui.c:
    	* extensions/livehttpheaders/livehttpheaders-ui.h:
    	* extensions/livehttpheaders/livehttpheaders-ui.glade:
    	* extensions/livehttpheaders/Makefile.am:
    	* extensions/livehttpheaders/mozilla/LiveHTTPHeadersListener.cpp:
    	* extensions/livehttpheaders/mozilla/LiveHTTPHeadersListener.h:
    	* extensions/livehttpheaders/mozilla/mozilla-helper.cpp:
    	* extensions/livehttpheaders/mozilla/mozilla-helper.h:
    	* extensions/livehttpheaders/mozilla/Makefile.am:
    
    	New Live HTTP Headers extension.

---
+++ extensions/livehttpheaders/ephy-livehttpheaders-extension.h
@@ -0,0 +1,60 @@
+/*
+ *  Copyright (C) 2003 Marco Pesenti Gritti
+ *  Copyright (C) 2003 Christian Persch
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#ifndef EPHY_LIVEHTTPHEADERS_EXTENSION_H
+#define EPHY_LIVEHTTPHEADERS_EXTENSION_H
+
+#include <glib.h>
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+#define EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION		(ephy_livehttpheaders_extension_get_type ())
+#define EPHY_LIVEHTTPHEADERS_EXTENSION(o)		(G_TYPE_CHECK_INSTANCE_CAST ((o), EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION, EphyLivehttpheadersExtension))
+#define EPHY_LIVEHTTPHEADERS_EXTENSION_CLASS(k)		(G_TYPE_CHECK_CLASS_CAST((k), EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION, EphyLivehttpheadersExtensionClass))
+#define EPHY_IS_LIVEHTTPHEADERS_EXTENSION(o)		(G_TYPE_CHECK_INSTANCE_TYPE ((o), EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION))
+#define EPHY_IS_LIVEHTTPHEADERS_EXTENSION_CLASS(k)	(G_TYPE_CHECK_CLASS_TYPE ((k), EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION))
+#define EPHY_LIVEHTTPHEADERS_EXTENSION_GET_CLASS(o)	(G_TYPE_INSTANCE_GET_CLASS ((o), EPHY_TYPE_LIVEHTTPHEADERS_EXTENSION, EphyLivehttpheadersExtensionClass))
+
+typedef struct _EphyLivehttpheadersExtension		EphyLivehttpheadersExtension;
+typedef struct _EphyLivehttpheadersExtensionClass	EphyLivehttpheadersExtensionClass;
+typedef struct _EphyLivehttpheadersExtensionPrivate	EphyLivehttpheadersExtensionPrivate;
+
+struct _EphyLivehttpheadersExtensionClass
+{
+	GObjectClass parent_class;
+};
+
+struct _EphyLivehttpheadersExtension
+{
+	GObject parent_instance;
+
+	/*< private >*/
+	EphyLivehttpheadersExtensionPrivate *priv;
+};
+
+GType	ephy_livehttpheaders_extension_get_type		(void);
+
+GType	ephy_livehttpheaders_extension_register_type	(GTypeModule *module);
+
+G_END_DECLS
+
+#endif
---
+++ extensions/livehttpheaders/mozilla/mozilla-helper.cpp
@@ -0,0 +1,72 @@
+/*
+ *  Copyright (C) 2006 Jean-François Rameau
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  $Id$
+ */
+
+#include "mozilla-config.h"
+
+#include "config.h"
+
+#include "mozilla-helper.h"
+#include "LiveHTTPHeadersListener.h"
+
+#include <nsIObserverService.h>
+#include <nsCOMPtr.h>
+#include <nsIServiceManager.h>
+#include <nsIHttpProtocolHandler.h>
+#include <nsServiceManagerUtils.h>
+
+static LiveHTTPHeadersListener *listener;
+
+extern "C" void
+livehttpheaders_register_listener (void)
+{
+	nsresult rv;
+
+	nsCOMPtr<nsIObserverService> observerService = do_GetService ("@mozilla.org/observer-service;1", &rv);
+	g_return_if_fail (NS_SUCCEEDED (rv));
+
+	listener = new LiveHTTPHeadersListener ();
+
+	rv = observerService->AddObserver (listener, NS_HTTP_ON_MODIFY_REQUEST_TOPIC, PR_FALSE);
+	rv = observerService->AddObserver (listener, NS_HTTP_ON_EXAMINE_RESPONSE_TOPIC, PR_FALSE);
+}
+
+extern "C" void
+livehttpheaders_unregister_listener (void)
+{
+	nsresult rv;
+
+	nsCOMPtr<nsIObserverService> observerService = do_GetService ("@mozilla.org/observer-service;1", &rv);
+	g_return_if_fail (NS_SUCCEEDED (rv));
+
+	rv = observerService->RemoveObserver (listener, NS_HTTP_ON_MODIFY_REQUEST_TOPIC);
+	rv = observerService->RemoveObserver (listener, NS_HTTP_ON_EXAMINE_RESPONSE_TOPIC);
+}
+
+extern "C" GSList *
+get_frames (void)
+{
+	return listener->GetFrames ();
+}
+
+extern "C" void
+clear_frames (void)
+{
+	return listener->ClearFrames ();
+}
