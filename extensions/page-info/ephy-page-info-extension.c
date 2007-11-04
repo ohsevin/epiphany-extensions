ohsevin/epiphany-extensions

commit 0323edb368765a88e8850d2149a578064a7fe647

Update page-info to work with epiphany trunk. Fixes bug #492928. Clean up trailing whitespaces and indentation.
svn path=/trunk/; revision=1632

extensions/page-info/ephy-page-info-extension.c
@@ -106,7 +106,7 @@ ephy_page_info_extension_register_type (GTypeModule *module)
 					    G_TYPE_OBJECT,
 					    "EphyPageInfoExtension",
 					    &our_info, 0);
-	
+
 	g_type_module_add_interface (module,
 				     type,
 				     EPHY_TYPE_EXTENSION,
@@ -125,7 +125,7 @@ ephy_page_info_extension_init (EphyPageInfoExtension *extension)
 
 static void
 ephy_page_info_extension_dialog_weak_notify_cb (
-	EphyPageInfoExtension *extension, 
+	EphyPageInfoExtension *extension,
 	GObject *dialog)
 {
 	extension->priv->page_info_dialogs = g_slist_remove
@@ -177,16 +177,16 @@ ephy_page_info_extension_display_cb (GtkAction *action,
 
 	LOG ("Creating page info dialog");
 
-	embed = ephy_window_get_active_embed (window);
+	embed = ephy_window_get_active_tab (embed);
 	g_return_if_fail (embed != NULL);
 
 	data = g_object_get_data (G_OBJECT (window), WINDOW_DATA_KEY);
 	g_return_if_fail (data != NULL);
-	
+
 	dialog = page_info_dialog_new (window, embed);
 
 	extension = data->extension;
-	extension->priv->page_info_dialogs =  
+	extension->priv->page_info_dialogs =
 			g_slist_append (extension->priv->page_info_dialogs, dialog);
 
 	g_object_weak_ref (G_OBJECT (dialog),
@@ -198,7 +198,7 @@ ephy_page_info_extension_display_cb (GtkAction *action,
 
 static void
 update_action (EphyWindow *window,
-	       EphyTab *tab)
+	       EphyEmbed *embed)
 {
 	GtkAction *action;
 	gboolean loading = TRUE;
@@ -206,18 +206,18 @@ update_action (EphyWindow *window,
 	action = gtk_ui_manager_get_action (GTK_UI_MANAGER (ephy_window_get_ui_manager (window)),
 					    MENU_PATH "/PageInfo");
 
-	g_object_get (G_OBJECT (tab), "load-status", &loading, NULL);
+	g_object_get (G_OBJECT (embed), "load-status", &loading, NULL);
 	g_object_set (G_OBJECT (action), "sensitive", !loading, NULL);
 }
 
 static void
-load_status_cb (EphyTab *tab,
+load_status_cb (EphyEmbed *embed,
 		GParamSpec *pspec,
 		EphyWindow *window)
 {
-	if (tab == ephy_window_get_active_tab (window))
+	if (embed == ephy_window_get_active_tab (embed))
 	{
-		update_action (window, tab);
+		update_action (window, embed);
 	}
 }
 
@@ -227,27 +227,30 @@ switch_page_cb (GtkNotebook *notebook,
 		guint page_num,
 		EphyWindow *window)
 {
+	EphyEmbed *embed;
+
 	if (GTK_WIDGET_REALIZED (window) == FALSE) return; /* on startup */
 
-	update_action (window, ephy_window_get_active_tab (window));
+	embed = ephy_window_get_active_tab (window);
+	update_action (window, embed);
 }
 
 static void
 impl_attach_tab (EphyExtension *extension,
 		 EphyWindow *window,
-		 EphyTab *tab)
+		 EphyEmbed *embed)
 {
-	g_signal_connect_after (tab, "notify::load-status",
+	g_signal_connect_after (embed, "notify::load-status",
 				G_CALLBACK (load_status_cb), window);
 }
 
 static void
 impl_detach_tab (EphyExtension *extension,
 		 EphyWindow *window,
-		 EphyTab *tab)
+		 EphyEmbed *embed)
 {
 	g_signal_handlers_disconnect_by_func
-		(tab, G_CALLBACK (load_status_cb), window);
+		(embed, G_CALLBACK (load_status_cb), window);
 }
 
 static void

extensions/page-info/mozilla/PageInfoPrivate.h
@@ -24,5 +24,5 @@ class nsIDOMDocument;
 
 namespace PageInfoPrivate
 {
-	EmbedPageRenderMode	GetRenderMode (nsIDOMDocument *aDocument);	
+	EmbedPageRenderMode	GetRenderMode (nsIDOMDocument *aDocument);
 }

extensions/page-info/mozilla/mozilla-helpers.cpp
@@ -112,8 +112,8 @@ private:
                     nsIDOMHTMLElement *aHTMLElement);
   void ProcessAppletNode (nsIDOMHTMLAppletElement *aElement);
   void ProcessAreaNode (nsIDOMHTMLAreaElement *aElement);
-  void ProcessEmbedNodeHelper (const nsString &aUrl, 
-  		               nsIDOMHTMLEmbedElement *aElement);
+  void ProcessEmbedNodeHelper (const nsString &aUrl,
+                               nsIDOMHTMLEmbedElement *aElement);
   void ProcessEmbedNode (nsIDOMHTMLEmbedElement *aElement);
   void ProcessFormNode (nsIDOMHTMLFormElement *aElement);
   void ProcessImageNode (nsIDOMHTMLImageElement *aElement);
@@ -141,7 +141,7 @@ private:
   GHashTable *mLinkHash;
   GHashTable *mFormHash;
   GList      *mMetaTagsList;
-  
+
   /* these two are used by the walker: */
   nsCString mDocCharset;
   nsCOMPtr<nsIURI> mBaseURI;
@@ -167,7 +167,7 @@ nsresult
 PageInfoHelper::Init (EphyEmbed *aEmbed)
 {
   NS_ENSURE_ARG (aEmbed);
-  
+
   nsCOMPtr<nsIWebBrowser> browser;
   gtk_moz_embed_get_nsIWebBrowser (GTK_MOZ_EMBED (aEmbed),
                                    getter_AddRefs (browser));
@@ -219,7 +219,7 @@ PageInfoHelper::Init (EphyEmbed *aEmbed)
 }
 
 /* A fairly good case insensitive comparison */
-static gint 
+static gint
 utf8_strcasecmp (const gchar *a, const gchar *b)
 {
 	gint retval;
@@ -235,8 +235,8 @@ utf8_strcasecmp (const gchar *a, const gchar *b)
 }
 
 /* Compares two links */
-static gint 
-link_compare (gconstpointer a, 
+static gint
+link_compare (gconstpointer a,
 	      gconstpointer b)
 {
 	EmbedPageLink *link_a = (EmbedPageLink *)a;
@@ -246,8 +246,8 @@ link_compare (gconstpointer a,
 }
 
 /* Compares two media, first by type then by url */
-static gint 
-media_compare (gconstpointer a, 
+static gint
+media_compare (gconstpointer a,
 	       gconstpointer b)
 {
 	gint res;
@@ -264,27 +264,27 @@ media_compare (gconstpointer a,
 	{
 		// Enum comparison ...
 		res = media_a->type > media_b->type ? 1 : -1;
-   	}
+	}
 
 	return res;
 }
 
 /* Compares two forms */
-static gint 
-form_compare (gconstpointer a, 
+static gint
+form_compare (gconstpointer a,
 	      gconstpointer b)
 {
 	EmbedPageForm *form_a = (EmbedPageForm *)a;
 	EmbedPageForm *form_b = (EmbedPageForm *)b;
 
-	// be careful, form's name may be NULL	
+	// be careful, form's name may be NULL
 	if (form_a->name == NULL)
 	{
-		return form_b->name == NULL ? 0 : -1; 
+		return form_b->name == NULL ? 0 : -1;
 	}
 	else if (form_b->name == NULL)
 	{
-		return form_a->name == NULL ? 0 : 1; 
+		return form_a->name == NULL ? 0 : 1;
 	}
 	return utf8_strcasecmp (form_a->name, form_b->name);
 }
@@ -338,7 +338,7 @@ PageInfoHelper::GetCacheEntryDescriptor (const nsAString &aUrl,
   nsresult rv = NS_OK;
 
   *aEntry = nsnull;
- 
+
   nsCOMPtr<nsICacheService> cacheService =
           do_GetService(NS_CACHESERVICE_CONTRACTID);
   NS_ENSURE_TRUE (cacheService, NS_ERROR_FAILURE);
@@ -358,11 +358,11 @@ PageInfoHelper::GetCacheEntryDescriptor (const nsAString &aUrl,
                                    PR_TRUE,
                                    getter_AddRefs (cacheSession));
       NS_ENSURE_TRUE (cacheSession, NS_ERROR_FAILURE);
-  
+
       cacheSession->SetDoomEntriesIfExpired (PR_FALSE);
-  
+
       nsCOMPtr<nsICacheEntryDescriptor> cacheEntryDescriptor;
-  
+
       rv = cacheSession->OpenCacheEntry (nsCString(url), nsICache::ACCESS_READ,
                                          PR_FALSE, aEntry);
 
@@ -444,9 +444,9 @@ PageInfoHelper::GetProperties ()
     {
       rv = domHtmlDoc->GetReferrer (value);
       if (NS_SUCCEEDED (rv) && value.Length ())
-      	{
+        {
           props->referring_url = ToCString (value);
-	}
+        }
     }
 
   /* Until https://bugzilla.mozilla.org/show_bug.cgi?id=154359 is fixed */
@@ -515,7 +515,7 @@ PageInfoHelper::ProcessNode (nsIDOMElement *aElement,
 
   /* check if we have it already */
   if (g_hash_table_lookup (mLinkHash, cUrl.get())) return;
-      
+
   EmbedPageLink *link = g_new0 (EmbedPageLink, 1);
   link->url = g_strdup (cUrl.get());
   g_hash_table_insert (mLinkHash, link->url, link);
@@ -546,7 +546,7 @@ PageInfoHelper::ProcessAppletNode (nsIDOMHTMLAppletElement *aElement)
   nsCString cUrl;
   rv = Resolve (tmp, cUrl);
   if (NS_FAILED (rv) || !cUrl.Length()) return;
-  
+
   if (g_hash_table_lookup (mMediaHash, cUrl.get())) return;
 
   /* FIXME: link or medium('media') ? */
@@ -596,8 +596,8 @@ PageInfoHelper::ProcessAreaNode (nsIDOMHTMLAreaElement *aElement)
 }
 
 void
-PageInfoHelper::ProcessEmbedNodeHelper (const nsString &aUrl, 
-  				        nsIDOMHTMLEmbedElement *aElement)
+PageInfoHelper::ProcessEmbedNodeHelper (const nsString &aUrl,
+                                        nsIDOMHTMLEmbedElement *aElement)
 {
   nsresult rv;
 
@@ -638,7 +638,7 @@ PageInfoHelper::ProcessEmbedNode (nsIDOMHTMLEmbedElement *aElement)
   // Look at href tag
   aElement->GetAttribute(mHrefAttr, tmp);
   if (NS_SUCCEEDED (rv) && tmp.Length())
-    { 
+    {
       ProcessEmbedNodeHelper (tmp, aElement);
     }
 }
@@ -964,7 +964,7 @@ PageInfoHelper::WalkTree (nsIDOMDocument *aDocument)
 
   nsCOMPtr<nsIDOMNode> aNode;
   nsCOMPtr<nsIDOMCSSStyleDeclaration> computedStyle;
-  
+
   walker->GetCurrentNode(getter_AddRefs(aNode));
   while (aNode)
     {
@@ -979,31 +979,30 @@ PageInfoHelper::WalkTree (nsIDOMDocument *aDocument)
               defaultCSSView->GetComputedStyle(nodeAsElement, EmptyString,
                                                getter_AddRefs (computedStyle));
             }
-  
+
           if (computedStyle)
             {
-	      nsCOMPtr<nsIDOMCSSValue> value;
-	      computedStyle->GetPropertyCSSValue(mBgImageAttr, getter_AddRefs (value));
+              nsCOMPtr<nsIDOMCSSValue> value;
+              computedStyle->GetPropertyCSSValue(mBgImageAttr, getter_AddRefs (value));
 
-	      nsCOMPtr<nsIDOMCSSPrimitiveValue> primitiveValue (do_QueryInterface (value));
-	      if (primitiveValue)
-	      	{
-		  PRUint16 primitiveType = 0;
-		  rv = primitiveValue->GetPrimitiveType (&primitiveType);
-		  if (NS_SUCCEEDED (rv) && primitiveType == nsIDOMCSSPrimitiveValue::CSS_URI)
-		    {
-		      nsString stringValue;
-		      rv = primitiveValue->GetStringValue (stringValue);
-		      if (NS_SUCCEEDED (rv) && stringValue.Length())
-		      	{
+              nsCOMPtr<nsIDOMCSSPrimitiveValue> primitiveValue (do_QueryInterface (value));
+              if (primitiveValue)
+                {
+                  PRUint16 primitiveType = 0;
+                  rv = primitiveValue->GetPrimitiveType (&primitiveType);
+                  if (NS_SUCCEEDED (rv) && primitiveType == nsIDOMCSSPrimitiveValue::CSS_URI)
+                    {
+                      nsString stringValue;
+                      rv = primitiveValue->GetStringValue (stringValue);
+                      if (NS_SUCCEEDED (rv) && stringValue.Length())
+                        {
                           EmbedPageMedium *medium = g_new0 (EmbedPageMedium, 1);
                           medium->type = MEDIUM_BG_IMAGE;
                           medium->url = ToCString (stringValue);
                           g_hash_table_insert(mMediaHash, medium->url, medium);
-			}
-		    }
-		  
-		}
+                        }
+                    }
+                }
             }
         }
 
@@ -1112,7 +1111,7 @@ PageInfoHelper::WalkFrame (nsIDOMDocument *aDocument)
 {
   nsCString saveCharset (mDocCharset);
   nsCOMPtr<nsIURI> uri (mBaseURI);
-  
+
   WalkTree (aDocument);
 
   mDocCharset = saveCharset;

extensions/page-info/mozilla/mozilla-helpers.h
@@ -72,7 +72,7 @@ typedef struct
 
 /* take care to sort enum by name */
 /* so we can sort media as well */
-typedef enum 
+typedef enum
 {
   MEDIUM_APPLET,
   MEDIUM_EMBED,
@@ -91,7 +91,7 @@ typedef struct
   char *title;
   int width;
   int height;
-} EmbedPageMedium; 
+} EmbedPageMedium;
 
 typedef struct
 {

commit 85b79282c004f007c6ae600917881c1e73d40ec6

Fix typos (embed versus window) introduced in r1632. Really fixes bug #492928. Adapt the handling of the title of ephy_embed_get_title() (which is now const).
svn path=/trunk/; revision=1633

extensions/page-info/ephy-page-info-extension.c
@@ -177,7 +177,7 @@ ephy_page_info_extension_display_cb (GtkAction *action,
 
 	LOG ("Creating page info dialog");
 
-	embed = ephy_window_get_active_tab (embed);
+	embed = ephy_window_get_active_tab (window);
 	g_return_if_fail (embed != NULL);
 
 	data = g_object_get_data (G_OBJECT (window), WINDOW_DATA_KEY);
@@ -215,7 +215,7 @@ load_status_cb (EphyEmbed *embed,
 		GParamSpec *pspec,
 		EphyWindow *window)
 {
-	if (embed == ephy_window_get_active_tab (embed))
+	if (embed == ephy_window_get_active_tab (window))
 	{
 		update_action (window, embed);
 	}

extensions/page-info/page-info-dialog.c
@@ -773,6 +773,7 @@ general_info_page_fill (InfoPage *page)
 	const char *date_hack = "%c"; /* quiet gcc */
 	char date[128];
 	char *val;
+	const char *title;
 	struct tm tm;
 	time_t t;
 
@@ -781,9 +782,8 @@ general_info_page_fill (InfoPage *page)
 	props = dialog->priv->page_info->props;
 	g_return_if_fail (props != NULL);
 
-	val = ephy_embed_get_title (embed);
-	page_info_set_text (dialog, properties[PROP_GENERAL_PAGE_TITLE].id, val);
-	g_free (val);
+	title = ephy_embed_get_title (embed);
+	page_info_set_text (dialog, properties[PROP_GENERAL_PAGE_TITLE].id, title);
 
 	val = ephy_embed_get_location (embed, TRUE);
 	page_info_set_text (dialog, properties[PROP_GENERAL_URL].id, val);